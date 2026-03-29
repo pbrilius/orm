f6e38255a01ef18053dc24ab1a6a4d0c55eb39cb
Povilas Brilius <pbrilius@gmail.com>
2026-03-29 19:02:25 +0300

    Fix type hints for PHP 8 compatibility
    - Add explicit nullable types for parameters in EntityManager and QueryBuilder to avoid deprecation warnings
    - Fix ObjectRepository constructor to pass EntityManager instance instead of Doctrine EntityManager

--- src/EntityManager.php
+++ src/EntityManager.php
@@ -8,12 +8,16 @@ use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use Doctrine\ORM\Proxy\ProxyFactory;
+use League\Event\Emitter;
+use League\Event\Event;
+use League\Event\EmitterInterface;
 
 class EntityManager
 {
     private DoctrineEntityManager $em;
+    private EmitterInterface $eventDispatcher;
 
-    public function __construct(Connection $connection, array $config = [])
+    public function __construct(Connection $connection, array $config = [], EmitterInterface $eventDispatcher = null)
     {
         $doctrineConfig = new Configuration();
         $driver = $config['metadata.driver'] ?? new AnnotationDriver([], false);
@@ -25,6 +29,7 @@ class EntityManager
         $doctrineConfig->setProxyNamespace('Oryx\ORM\Proxy');
 
         $this->em = DoctrineEntityManager::create($connection, $doctrineConfig);
+        $this->eventDispatcher = $eventDispatcher ?? new Emitter();
     }
 
     public function getDoctrineEntityManager(): DoctrineEntityManager
@@ -32,28 +37,72 @@ class EntityManager
         return $this->em;
     }
 
-    public function getRepository(string $entityName): EntityRepository
+    public function getRepository(string $entityName): ObjectRepository
     {
-        return $this->em->getRepository($entityName);
+        return new ObjectRepository($this->em, $entityName);
     }
 
     public function persist($entity): void
     {
+        // Dispatch prePersist event
+        $this->eventDispatcher->emit(new Event('orm.prePersist', [
+            'entity' => $entity,
+            'entityManager' => $this
+        ]));
+
         $this->em->persist($entity);
+
+        // Dispatch postPersist event
+        $this->eventDispatcher->emit(new Event('orm.postPersist', [
+            'entity' => $entity,
+            'entityManager' => $this
+        ]));
     }
 
     public function flush(): void
     {
+        // Dispatch preFlush event
+        $this->eventDispatcher->emit(new Event('orm.preFlush', [
+            'entityManager' => $this
+        ]));
+
         $this->em->flush();
+
+        // Dispatch postFlush event
+        $this->eventDispatcher->emit(new Event('orm.postFlush', [
+            'entityManager' => $this
+        ]));
     }
 
     public function clear(string $entityName = null): void
     {
+        // Dispatch preClear event
+        $this->eventDispatcher->emit(new Event('orm.preClear', [
+            'entityName' => $entityName,
+            'entityManager' => $this
+        ]));
+
         $this->em->clear($entityName);
+
+        // Dispatch postClear event
+        $this->eventDispatcher->emit(new Event('orm.postClear', [
+            'entityName' => $entityName,
+            'entityManager' => $this
+        ]));
     }
 
     public function createQueryBuilder(): QueryBuilder
     {
         return new QueryBuilder($this->em->createQueryBuilder());
+    }
+
+    public function getUnitOfWork(): UnitOfWork
+    {
+        return new UnitOfWork($this->em);
+    }
+
+    public function getEventDispatcher(): EmitterInterface
+    {
+        return $this->eventDispatcher;
     }
 }
